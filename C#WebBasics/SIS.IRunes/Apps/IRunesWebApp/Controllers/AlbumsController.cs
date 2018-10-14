2ffea6f78cfef04cb16017eee9ee7b9167d2948a
klubenov <pwnd0rum@gmail.com>
2018-10-14 23:11:12 +0300

    Upload IRunesApp.

---
+++ C#WebBasics/SIS.IRunes/Apps/IRunesWebApp/Controllers/AlbumsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using IRunesWebApp.Models;
+using Services;
+using SIS.HTTP.Cookies;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.WebServer.Results;
+
+namespace IRunesWebApp.Controllers
+{
+    public class AlbumsController : BaseController
+    {
+        public IHttpResponse All(IHttpRequest request)
+        {
+            if (!this.IsAuthenticated(request))
+            {
+                return new RedirectResult("/users/login");
+            }
+
+            var albums = this.Context.Albums;
+
+            if (albums.Any())
+            {
+                var listOfAlbums = string.Empty;
+                foreach (var album in albums)
+                {
+                   var albumHtml = $@"<p><a href=""/albums/details/{album.Id}"">{album.Name}</a></p>";
+                    listOfAlbums += albumHtml;
+                }
+                this.ViewBag["albumsList"] = listOfAlbums;
+            }
+            else
+            {
+                this.ViewBag["albumsList"] = "There are currently no albums.";
+            }
+
+            return this.View();
+        }
+
+        public IHttpResponse Create(IHttpRequest request)
+        {
+            if (!this.IsAuthenticated(request))
+            {
+                return new RedirectResult("/users/login");
+            }
+
+            return this.View();
+        }
+
+        public IHttpResponse PostCreate(IHttpRequest request)
+        {
+            var name = request.FormData["name"].ToString();
+            var cover = request.FormData["cover"].ToString();
+
+            var album = new Album
+            {
+                Name = name,
+                Cover = cover
+            };
+
+            this.Context.Albums.Add(album);
+
+            try
+            {
+                this.Context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                // TODO: Log error
+                return new BadRequestResult(
+                    e.Message,
+                    HttpResponseStatusCode.InternalServerError);
+            }
+
+            var response = new RedirectResult("/albums/all");
+
+            return response;
+        }
+    }
+}
---
+++ C#WebBasics/SIS.IRunes/Apps/IRunesWebApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using IRunesWebApp.Controllers;
+using SIS.HTTP.Enums;
+using SIS.WebServer;
+using SIS.WebServer.Results;
+using SIS.WebServer.Routing;
+
+namespace IRunesWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            ConfigureRouting(serverRoutingTable);
+
+
+            Server server = new Server(80, serverRoutingTable);
+
+            server.Run();
+        }
+
+        private static void ConfigureRouting(ServerRoutingTable serverRoutingTable)
+        {
+            // GET
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/home/index"] =
+                request => new RedirectResult("/");
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/"] =
+                request => new HomeController().Index(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/users/login"] =
+                request => new UsersController().Login(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/users/register"] =
+                request => new UsersController().Register(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/albums/all"] =
+                request => new AlbumsController().All(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/albums/create"] =
+                request => new AlbumsController().Create(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/users/logout"] =
+                request => new UsersController().Logout(request);
+
+
+            // POST
+            serverRoutingTable.Routes[HttpRequestMethod.Post]["/users/login"] =
+                request => new UsersController().PostLogin(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Post]["/users/register"] =
+                request => new UsersController().PostRegister(request);
+            serverRoutingTable.Routes[HttpRequestMethod.Post]["/albums/create"] =
+                request => new AlbumsController().PostCreate(request);
+        }
+    }
+}
